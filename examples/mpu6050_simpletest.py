de6c67ac3dda89aad1e4a638f813fb2b714a3079
Brendan <2bndy5@gmail.com>
2019-09-20 18:54:41 -0700

    my first experience with the map function

--- examples/mpu6050_simpletest.py
+++ examples/mpu6050_simpletest.py
@@ -1,22 +1,23 @@
 """
 A simple test of the MPU6050 6D0F sensor
 """
+# pylint: disable=invalid-name
 import time
 import board
 import busio
 from circuitpython_mpu6050 import MPU6050
 I2C_BUS = busio.I2C(board.SCL, board.SDA)
-IMU_SENSOR = MPU6050(I2C_BUS)
+sensor = MPU6050(I2C_BUS)
 
 while True:
     try:
-        ACCEL = IMU_SENSOR.acceleration
-        TEMP = IMU_SENSOR.temperature
-        GYRO = IMU_SENSOR.gyro
-        print('temp =', TEMP)
-        print('accel =', repr(ACCEL))
-        print('gyro =', repr(GYRO))
+        accel_x, accel_y, accel_z = sensor.acceleration
+        temp = sensor.temperature
+        gyro_x, gyro_y, gyro_z = sensor.gyro
+        print('temp =', temp)
+        print('accel =', accel_x, accel_y, accel_z)
+        print('gyro =', gyro_x, gyro_y, gyro_z)
         time.sleep(2)
     except KeyboardInterrupt:
-        del IMU_SENSOR
+        del sensor
         break
